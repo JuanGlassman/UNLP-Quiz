d66d4c42fb0aa49992565c8982c0b6df071941b5
JuaniGlassman <juanignacioglassman@gmail.com>
2025-03-05 14:05:57 -0300

    Preguntas DBD y alertas

--- API/views.py
+++ API/views.py
@@ -22,45 +22,35 @@ def quiz_view(request):
     current_index = int(request.session.get('current_index', 0))
     correct_answers = int(request.session.get('correct_answers', 0))
 
-
     if current_index >= total_questions:
-        request.session.flush()  # Limpiar la sesión para reiniciar el quiz
+        request.session.flush() 
         return render(request, 'quiz_result.html', {'score': correct_answers, 'total': total_questions})
 
     current_question = questions[current_index]
 
     is_correct = None
     selected_answer = None
+    show_feedback = False
     error_message = None
 
     if request.method == 'POST':
         selected_answer = request.POST.get('answer')
 
-        error_message = None
-        if not selected_answer:
-            error_message = "⚠️ Debes seleccionar una respuesta antes de continuar."
-        else:
+        if selected_answer:
             correct_answer = current_question['answer']
-            error_message = None
 
             is_correct = (selected_answer == correct_answer)
             if is_correct:
                 request.session['correct_answers'] = correct_answers + 1
 
             request.session['current_index'] = current_index + 1
-
-            
-            return render(request, 'Quiz.html', {
-                'question': current_question,
-                'selected_answer': selected_answer,
-                'is_correct': is_correct,
-                'show_feedback': True,
-                'error_message': error_message, 
-            })
         
+            show_feedback = True
     
     return render(request, 'quiz.html', {
         'question': current_question,
-        'error_message': error_message, 
-        'show_feedback': False,
+        'options': current_question['options'],
+        'show_feedback': show_feedback,
+        'is_correct': is_correct,
+        'selected_answer': selected_answer,
     })
